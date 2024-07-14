94d35c34c6ff482864b6dd7c1d5c2f246397effb
ahmad <ranagee444732@gmail.com>
2024-07-15 00:04:09 +0500

    dad

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
@@ -23,19 +22,21 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
+// Enable CORS for specific origins
 const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
+  origin: ["http://localhost:3001"],
+  credentials: true, // Required if you're using cookies, authorization headers, etc.
+  optionSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use("/", routes);
+
+// Default route example
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
