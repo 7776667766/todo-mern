f40abcf56a8284b214700e0a8bbbb2166a322859
ahmad <ranagee444732@gmail.com>
2024-07-15 10:58:10 +0500

    up

--- index.js
+++ index.js
@@ -5,40 +5,36 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// Routes
-const routes = require("./routes/index");
-const path = require("path");
-
-
+// Database connection
 const dbConnect = require("./db/dbconnect");
 dbConnect();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
-// Enable CORS for specific origins
 const corsOptions = {
-  origin: ["http://localhost:3000"],
-  credentials: true, // Required if you're using cookies, authorization headers, etc.
-  optionSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: "*",
+  withcredentials: true,
+  optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
+const routes = require("./routes/index");
 app.use("/", routes);
 
 // Default route example
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
